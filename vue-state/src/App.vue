1fd05528bdb0906a3ad9d0b19113490792bf7a3a
Dan Høegh <dan.hoegh@gmail.com>
2021-11-15 13:31:47 +0100

    lesson version 2
    This reverts commit 550c3962ebf190b351f9a26bbeb1c83d48d0bb24.

--- vue-state/src/App.vue
+++ vue-state/src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <div>
-    <h1>To-Do List</h1>
-  </div>
 </template>
+
 <script>
-
-
-export default {
-
-};
 </script>
 
 <style>
---
+++ vue-state/src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+li {
+  font-size: 1.2rem;
+}
+button {
+  margin-left: 2rem;
+  border: none;
+  background-color: #669966;
+  padding: 0.3rem 1rem;
+  color: #fff;
+  display: inline-block;
+}
+</style>
